ae15c9b3eaa33aa42f2333d1b80bf060b5d7c146
Mocanu Mihai <mocanunmihai@gmail.com>
2023-12-12 19:57:49 +0200

    Delete the new list button

--- src/pages/ToDo/Form.jsx
+++ src/pages/ToDo/Form.jsx
@@ -1,40 +1,39 @@
-import { useState } from "react"
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList';
+import { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import FeaturedPlayListIcon from "@material-ui/icons/FeaturedPlayList";
 
+export function Form({ addToDo }) {
+  const [newItem, setNewItem] = useState("");
 
-export function Form({addToDo}) {
-
-    const [newItem, setNewItem] = useState("")
-
-
-    
-function handleClick(e){
-    e.preventDefault()
-    addToDo(newItem)
-    setNewItem("")
+  function handleClick(e) {
+    e.preventDefault();
+    addToDo(newItem);
+    setNewItem("");
   }
 
-  function handleAddList(e){
-    e.preventDefault()
-    
-  }
-  
-    return (
-    <form  className="create-todo" >
-        <textarea
-        type='text' 
-        id='newItem' 
+  // function handleAddList(e){
+  //   e.preventDefault()
+
+  // }
+
+  return (
+    <form className="create-todo">
+      <textarea
+        type="text"
+        id="newItem"
         placeholder="Add your todos..."
-        onChange = {(e)=>setNewItem(e.target.value)} 
-        value={newItem} 
+        onChange={(e) => setNewItem(e.target.value)}
+        value={newItem}
         autoFocus
         autoComplete="off"
-        />
-        <Fab  className="todoBtn1" onClick={handleClick}><AddIcon /></Fab>
-        <Fab className="todoBtn2" onClick={handleAddList}><FeaturedPlayListIcon /></Fab>
+      />
+      <Fab className="todoBtn1" onClick={handleClick}>
+        <AddIcon />
+      </Fab>
+      {/* <Fab className="todoBtn2" onClick={handleAddList}>
+        <FeaturedPlayListIcon />
+      </Fab> */}
     </form>
-    )
-  
+  );
 }
